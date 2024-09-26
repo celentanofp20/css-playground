1276696bef55054d0bedf4559db244609aefc3ff
celentanofp20 <frankie.celentano@yahoo.com>
2024-09-26 18:46:52 -0400

    styling chnages

--- app/photo/page.tsx
+++ app/photo/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         </div>
 
         {/* Welcome */}
-        <section className={styles.welcomeSection}>
+        <section className={`${styles.welcomeSection}`}>
           <div className={styles.textImageSection}>
             <div className={styles.textContent}>
               <h2>Welcome to Estate Frame Media</h2>
@@ -21,7 +21,7 @@ export default function Home() {
                 Capture Your Properties in Stunning Detail
               </p>
               <div className={styles.buttonGroup}>
-                <a href="#contact" className={styles.scrollButton}>Contact Us</a>
+                <a href="#contact" className={styles.scrollButton}>Book Now</a>
                 <a href="#services" className={styles.scrollButton}>Learn More</a>
               </div>
             </div>
@@ -35,7 +35,7 @@ export default function Home() {
         <section className={styles.aboutSection}>
           <h2>About Us</h2>
           <p>
-            Welcome to <strong>Esate Frame Media</strong>, where we specialize in premium real estate photography and videography. Serving real estate agents, brokerages, and commercial businesses, we deliver high-quality images and videos that showcase your properties in the best light. From striking interior HDR photos to breathtaking aerial drone footage, our professional and friendly team ensures your listings stand out.
+            Welcome to <strong>Estate Frame Media</strong>, where we specialize in premium real estate photography and videography. Serving real estate agents, brokerages, and commercial businesses, we deliver high-quality images and videos that showcase your properties in the best light. From striking interior HDR photos to breathtaking aerial drone footage, our professional and friendly team ensures your listings stand out.
           </p>
           <h3>Our Expertise Includes:</h3>
           <ul className={styles.expertiseList}>
@@ -53,16 +53,19 @@ export default function Home() {
           <h2>Services</h2>
           <div className={styles.gridContainer}>
             <div className={styles.gridItem}>
-              <img src="/interior-bathroom.jpg" alt="Interior Bathroom" />
-              <p>Interior and Exterior HDR Photography Our HDR photography captures every detail of your property, from the grandeur of the living room to the charm of the exterior. With our professional-grade equipment and techniques, your property will shine in every shot.</p>
+              <img className='space-s' src="/interior-bathroom.jpg" alt="Interior Bathroom" />
+              <strong>Interior and Exterior HDR Photography</strong>
+              <p className='space-n space-ew'>Our HDR photography captures every detail of your property, from the grandeur of the living room to the charm of the exterior. With our professional-grade equipment and techniques, your property will shine in every shot.</p>
             </div>
             <div className={styles.gridItem}>
-              <img src="/aerial-pool.jpg" alt="Aerial Pool" />
-              <p>Aerial Drone Photography and Videography Elevate your property listings with our drone services. We provide high-resolution aerial images and dynamic video footage that offer a comprehensive view of your property and its surroundings.</p>
+              <img className='space-s' src="/aerial-pool.jpg" alt="Aerial Pool" />
+              <strong>Aerial Drone Photography and Videography</strong>
+              <p className='space-n space-ew'>Elevate your property listings with our drone services. We provide high-resolution aerial images and dynamic video footage that offer a comprehensive view of your property and its surroundings.</p>
             </div>
             <div className={styles.gridItem}>
-              <img src="/interior-living-room.jpg" alt="Interior Living Room" />
-              <p>Interior Video Tours Give potential buyers an immersive experience with our interior video tours. These professionally crafted videos allow viewers to explore your property as if they were there, enhancing their connection and interest.
+              <img className='space-s' src="/interior-living-room.jpg" alt="Interior Living Room" />
+              <strong>Interior Video Tours</strong>
+              <p className='space-n space-ew'>Give potential buyers an immersive experience with our interior video tours. These professionally crafted videos allow viewers to explore your property as if they were there, enhancing their connection and interest.
               </p>
             </div>
           </div>
@@ -73,9 +76,9 @@ export default function Home() {
           <div className={styles.content}>
             <img src="/marketing-kit.jpg" alt="Social Media Kit" className={styles.kitImage} />
             <div className={styles.text}>
-              <h2>HD Photo Hub Marketing Kit</h2>
+              <h2>Social Media Kit</h2>
               <p>
-                Our HD Photo Hub Marketing Kit offers a comprehensive solution for real estate professionals looking to elevate their marketing efforts. With high-resolution images, custom branding, and expert consultation, you’ll have everything you need to create compelling property listings that attract attention and drive results.
+                Our <strong>Social Media Kit</strong> offers a comprehensive solution for real estate professionals looking to elevate their marketing efforts. With high-resolution images, custom branding, and expert consultation, you’ll have everything you need to create compelling property listings that attract attention and drive results.
               </p>
             </div>
           </div>
---
+++ app/practice/page.tsx
@@ -0,0 +1,8 @@
+import styles from './practice.module.css';
+
+export default function Home() {
+  return (
+    <>
+    </>
+  );
+}
